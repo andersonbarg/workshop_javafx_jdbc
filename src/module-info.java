0d6655352ff51891393ab6b5df648d19ec79da56
Anderson Barg <andbarg@yahoo.com.br>
2025-08-07 15:23:27 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens gui to javafx.fxml;
 	opens model.entities to javafx.base;
